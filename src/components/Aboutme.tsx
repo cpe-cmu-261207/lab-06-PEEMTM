f4ff501b2463c075ad150f8712d82a32dd656212
PEEMTM <peem33302@gmail.com>
2021-08-29 21:25:18 +0700

    finish

---
+++ src/components/Aboutme.tsx
@@ -0,0 +1,10 @@
+const Aboutme = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>Atthapong Auewongchai 630610770</p>
+        </div>
+    )
+}
+
+export default Aboutme
